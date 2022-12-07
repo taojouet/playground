677b0adfba6c66707de5ddea80f7e27c0bf8a838
taojouet <taojouet@gmail.com>
2022-12-07 17:18:41 +0100

    Add .next to gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c3211d96a497e85b"; }
+/******/ 		__webpack_require__.h = function() { return "fab794e6d73db4df"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
