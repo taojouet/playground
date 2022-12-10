c897154c85720c6ecc73cb25b09e0af9a034a2f2
taojouet <taojouet@gmail.com>
2022-12-10 16:02:23 +0100

    Add image and welcome text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,26 @@
 import CustomCursor from '../components/Cursor'
+import Image from 'next/image'
+import profilePic from '../public/img/tao.png'
 
 
 export default function Home() {
   return (
     <div>
-      <main className="min-h-screen p-64 flex-1 flex flex-col justify-center items-center">
+      <body>
         <CustomCursor />
-        <div className="">
-          <p className="text-center text-2xl text-bold">Welcome to my playground!</p>
-        </div>
-      </main>
+        <main className="min-h-screen p-64 flex-1 justify-center items-center">
+          <div className="container mx-auto p-4">
+            <div className="flex items-center">
+              <Image src={profilePic} alt="Picture of the author" width={5763} height={3842} className="w-1/3 mr-4" />
+              <p className="text-center w-2/3">
+                Hi! My name is Tao and I am a developer. This website will showcase my projects, my skills and anything I'd love to share with you.
+                <p className="font-semibold">Welcome to my playground!</p>
+              </p>
+
+            </div>
+          </div>
+        </main>
+      </body>
     </div>
   )
-}
+  }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,15 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                'custom-blue': '#9EB5EB',
+                transparent: 'transparent',
+                current: 'currentColor',
+                'white': '#ffffff',
+                'black': '#000000',
+                'dark-blue': '#264653',
+                'green': '#2A9D8F',
+                'yellow': '#E9C46A',
+                'orange': '#F4A261',
+                'dark-orange': '#E76F51'
             },
         },
     },
