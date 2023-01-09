e1bb29c60554d653b46fc47807d0237e53c78baf
taojouet <taojouet@gmail.com>
2023-01-09 19:47:20 +0100

    Add github and linkedin logos

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,32 @@
+import Image from 'next/image'
+
 export default function Footer() {
+
+
     return (
-        <footer>
+
+        <footer className="bg-slate-400 text-white py-4">
+            <div className="container mx-auto flex justify-around">
+
+                <a href="https://www.linkedin.com/in/taojouet/" target="_blank">
+                    <Image
+                        src="/../public/logo/linkedin.png"
+                        alt="Github page of Tao Jouet"
+                        width={32} height={32}
+                    />
+                </a>
+
+
+                <a href="https://github.com/taojouet" target="_blank">
+                    <Image
+                        src="/../public/logo/github.png"
+                        alt="Github page of Tao Jouet"
+                        width={32} height={32}
+                    />
+                </a>
+
+            </div>
         </footer>
+
     );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,19 +8,63 @@ export default function Home() {
     <div>
       <body>
         <CustomCursor />
-        <main className="min-h-screen p-64 flex-1 justify-center items-center bg-slate-400">
-          <div className="container mx-auto p-4">
-            <div className="flex items-center">
-              <Image src={profilePic} alt="Picture of the author" width={5763} height={3842} className="w-1/3 mr-4" />
-              <p className="text-center w-2/3">
-                Hi! My name is Tao and I am a developer. This website will showcase my projects, my skills and anything I'd love to share with you.
-                <p className="font-semibold text-yellow">Welcome to my playground!</p>
-              </p>
+        <main>
+
+          {/* FIRST SECTION START */}
+
+          <div className="min-h-screen p-64 flex-1 justify-center items-center bg-slate-400">
+            <div className="container mx-auto p-4">
+              <div className="flex items-center">
+                <Image src={profilePic} alt="Picture of the author" width={5763} height={3842} className="w-1/3 mr-4" />
+                <p className="text-center w-2/3">
+                  Hi! My name is Tao and I am a developer. This website will showcase my projects, my skills and anything I'd love to share with you.
+                  <br />
+                  <span className="font-semibold text-yellow">Welcome to my playground!</span>
+                </p>
+              </div>
             </div>
-            
           </div>
+          {/* FIRST SECTION END */}
+
+
+
+          {/* SECOND SECTION START */}
+          {/* <div className="min-h-screen p-64 flex-1 justify-center items-center bg-slate-400">
+            <div className="container mx-auto p-4">
+              <div className="flex items-center">
+                <div className="grid grid-cols-4 gap-4">
+                  <p className="text-center w-2/3">
+                    Hi! My name is Tao and I am a developer. This website will showcase my projects, my skills and anything I'd love to share with you.
+                    <br />
+                    <span className="font-semibold text-yellow">Welcome to my playground!</span>
+                  </p>
+
+                  <p className="text-center w-2/3">
+                    Hi! My name is Tao and I am a developer. This website will showcase my projects, my skills and anything I'd love to share with you.
+                    <br />
+                    <span className="font-semibold text-yellow">Welcome to my playground!</span>
+                  </p>
+
+                  <p className="text-center w-2/3">
+                    Hi! My name is Tao and I am a developer. This website will showcase my projects, my skills and anything I'd love to share with you.
+                    <br />
+                    <span className="font-semibold text-yellow">Welcome to my playground!</span>
+                  </p>
+
+                  <p className="text-center w-2/3">
+                    Hi! My name is Tao and I am a developer. This website will showcase my projects, my skills and anything I'd love to share with you.
+                    <br />
+                    <span className="font-semibold text-yellow">Welcome to my playground!</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div> */}
+          {/* SECOND SECTION END */}
+
+
         </main>
       </body>
     </div>
   )
-  }
+}
