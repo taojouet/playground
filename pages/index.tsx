598f5374f464702dcfb2a46ed81aa7770d4a7b6c
taojouet <taojouet@gmail.com>
2023-01-14 16:27:11 +0100

    Modify architecture for components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import CustomCursor from '../components/Cursor'
-import Image from 'next/image'
-import profilePic from '../public/img/tao.png'
-
+import TaoJouet from '../components/TaoJouet'
+import CV from '../components/CV'
 
 export default function Home() {
   return (
@@ -12,21 +11,11 @@ export default function Home() {
 
           {/* FIRST SECTION START */}
 
-          <div className="min-h-screen p-64 flex-1 justify-center items-center bg-slate-400">
-            <div className="container mx-auto p-4">
-              <div className="flex items-center">
-                <Image src={profilePic} alt="Picture of the author" width={5763} height={3842} className="w-1/3 mr-4" />
-                <p className="text-center w-2/3">
-                  Hi! My name is Tao and I am a developer. This website will showcase my projects, my skills and anything I'd love to share with you.
-                  <br />
-                  <span className="font-semibold text-yellow">Welcome to my playground!</span>
-                </p>
-              </div>
-            </div>
-          </div>
+          <TaoJouet />
+          
           {/* FIRST SECTION END */}
 
-
+          {/* <CV /> */}
 
           {/* SECOND SECTION START */}
           {/* <div className="min-h-screen p-64 flex-1 justify-center items-center bg-slate-400">
