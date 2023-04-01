aa258fd56fd5be9cf8e01695c343619d27b8cb39
taojouet <taojouet@gmail.com>
2023-04-01 15:40:29 +0200

    resize image

--- components/Presentation.tsx
+++ components/Presentation.tsx
@@ -6,7 +6,7 @@ export default function Presentation() {
         <div className="min-h-screen p-64 flex-1 justify-center items-center bg-slate-400">
             <div className="container mx-auto p-4">
               <div className="flex items-center">
-                <Image src={profilePic} alt="Picture of the author" width={5763} height={3842} className="w-1/3 mr-4" />
+                <Image src={profilePic} alt="Picture of the author" width={1280} height={720} className="w-1/3 mr-4" />
                 <p className="text-center w-2/3">
                   Hi! My name is Tao and I am a developer. This website will showcase my projects, my skills and anything I'd love to share with you.
                   <br />
