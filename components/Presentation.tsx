ef707249851d8df63454fdeaf510c636678a59fa
Tao Jouet <49972956+taojouet@users.noreply.github.com>
2023-04-30 17:53:48 +0200

    Change presentation component

--- components/Presentation.tsx
+++ components/Presentation.tsx
@@ -7,7 +7,7 @@ export default function Presentation(
   }
 ) {
   return (
-    <div className="min-h-screen p-64 flex-1 justify-center items-center bg-slate-400">
+    <div className="min-h-screen p-64 flex-1 justify-center items-center bg-slate-400"> 
       <div className="container mx-auto p-4">
         <div className="flex items-center">
           <Image src={profilePicture} alt="Picture of the author" width={720} height={480} className="w-1/3 mr-4" />
